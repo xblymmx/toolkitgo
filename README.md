8a0d7922993905f2e5dcf61389c215347fa3102d
xblymmx <tsmwymsmdyts@outlook.com>
2018-03-03 23:46:54 +0800

    init, add logger library

---
+++ README.md
@@ -0,0 +1,3 @@
+# golang toolkit
+
+## WIP
---
+++ logger/logger.go
@@ -0,0 +1,48 @@
+package logger
+
+import (
+	"log"
+	"os"
+	"io"
+	"strings"
+)
+
+var (
+	Info  *log.Logger
+	Error *log.Logger
+	Warning *log.Logger
+	Default *log.Logger // write to both default log-file and stdout
+)
+
+func init() {
+	errFile, err := os.OpenFile("error.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Cannot open error file failed:", err)
+	}
+	logFile, err := os.OpenFile("defaultlog.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Cannot open error file failed:", err)
+	}
+
+	Info = log.New(os.Stdout, "[Info]", log.Ldate|log.Ltime|log.Lshortfile)
+	Warning = log.New(os.Stdout, "[Warning]", log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(io.MultiWriter(os.Stderr, errFile), "[Error]", log.Ldate|log.Ltime|log.Lshortfile)
+	Default = log.New(logFile, "[Default]", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func NewFileLogger(prefix string, filename string, isWriteToStdout bool) *log.Logger {
+	if !strings.Contains(filename, ".") {
+		filename = filename + ".log"
+	}
+
+	logFile, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Cannot open error file failed:", err)
+	}
+
+	if isWriteToStdout {
+		return log.New(io.MultiWriter(os.Stdout, logFile), prefix, log.Ldate|log.Ltime|log.Lshortfile)
+	} else {
+		return log.New(logFile, prefix, log.Ldate|log.Ltime|log.Lshortfile)
+	}
+}
